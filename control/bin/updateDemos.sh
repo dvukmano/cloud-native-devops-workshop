bba23605927549f807b9cc00d3281ef771a1c849
Peter Nagy <nagypeter76@gmail.com>
2017-08-08 15:51:40 +0200

    kill packagekit process

--- control/bin/updateDemos.sh
+++ control/bin/updateDemos.sh
@@ -52,6 +52,8 @@ ${CONTENT_DIR}/control/bin/updateVM.sh
 
 echo "Clean yum cache..."
 
+ps -ef | grep PackageKit | grep -v grep | awk '{print $2}' | xargs -r kill -9
+
 sudo yum --enablerepo=* clean all
 
 read -p "Checkout complete. Press [Enter] to close the window"
