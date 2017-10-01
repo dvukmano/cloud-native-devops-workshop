4b950a8ddf6f613bccfd45284751f97bcdbcbb4f
Peter Nagy <nagypeter76@gmail.com>
2017-10-01 15:52:25 -0700

    create firefox icon, kill yum in background

--- control/bin/updateVM3.sh
+++ control/bin/updateVM3.sh
@@ -20,9 +20,37 @@ fi
 echo "http_proxy=$http_proxy"
 echo "https_proxy=$https_proxy"
 
+echo "========================================"
+FF_SHORTCUT=~/Desktop/chrome.desktop
+
+if grep -q "Exec=/usr/bin/firefox" $FF_SHORTCUT;
+then
+    echo "Firefox shortcut ready."    
+else
+    cat > $FF_SHORTCUT <<EOF
+[Desktop Entry]
+Comment[en_US]=This is Firefox
+Comment=This is Firefox
+Exec=/usr/bin/firefox
+GenericName[en_US]=Firefox
+GenericName=Firefox
+Icon=firefox
+MimeType=
+Name[en_US]=Firefox
+Name=Firefox
+Path=/tmp/
+StartupNotify=true
+Terminal=false
+TerminalOptions=
+Type=Application
+EOF
+    echo "Firefox shortcut has been created."
+fi
+echo "========================================"
+
 echo "Clean and disable yum cache/PackageKit..."
 
-sudo ps -ef | grep PackageKit | grep -v grep | awk '{print $2}' | xargs -r kill -9
+sudo ps -ef | grep PackageKit | grep -v grep | awk '{print $2}' | xargs -r kill -9 &
 
 sudo yum --enablerepo=* clean all
 
